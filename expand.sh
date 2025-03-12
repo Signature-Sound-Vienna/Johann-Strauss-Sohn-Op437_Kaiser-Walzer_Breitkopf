30b71d11dc298068e445a71eb5ac2c4888521270
Werner Goebl <goebl@mdw.ac.at>
2025-03-12 16:47:25 +0100

    Expand three version of the Walzer

---
+++ expand.sh
@@ -0,0 +1,3 @@
+verovio --expand 'Long_Version_Harnoncourt' -a -t mei -o Long_Version_Harnoncourt.mei Kaiser-Walzer_Breitkopf.mei
+verovio --expand 'Short_Version_Boskovsky' -a -t mei -o Short_Version_Boskovsky.mei Kaiser-Walzer_Breitkopf.mei
+verovio --expand 'Shortest_Version_Karajan_1946' -a -t mei -o Shortest_Version_Karajan_1946.mei Kaiser-Walzer_Breitkopf.mei
